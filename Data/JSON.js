9b58e1c1cb8a95634f35a602be7bfe888a875d95
Clarice0997 <clarice09975@gmail.com>
2023-03-19 19:19:16 +0800

    localStorage

--- Data/JSON.js
+++ Data/JSON.js
@@ -139,7 +139,7 @@ const user = {
 
 txt = JSON.stringify(user, ['firstName', 'lastName', 'country', 'city', 'age'], 2)
 console.log(txt)
-电脑
+
 // {
 //     "firstName": "Asabeneh",
 //     "lastName": "Yetayeh",
